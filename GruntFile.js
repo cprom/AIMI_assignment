fd746eae0ac543ac02a09cba4b22e6a53b0f696a
Chen Prom <cprom@Chens-MBP.lan>
2024-03-14 22:11:40 -0500

    update gruntfile to create dist folder

--- GruntFile.js
+++ GruntFile.js
@@ -1,27 +1,63 @@
+// module.exports = function(grunt) {
+//     grunt.initConfig({
+//       pkg: grunt.file.readJSON('package.json'),
+//       sass: {
+//         dist: {
+//           options: {
+//             sourcemap: false,
+//             compress: false,
+//             yuicompress: false,
+//             style: 'expanded',
+//           },
+//           files: {
+//             'src/js/scss/style.css' : 'src/js/scss/styles.scss'
+//           }
+//         },
+//       },
+//       watch: {
+//         css: {
+//           files: '**/*.scss',
+//           tasks: ['sass']
+//         }
+//       }
+//     });
+//     grunt.loadNpmTasks('grunt-contrib-sass');
+//     grunt.loadNpmTasks('grunt-contrib-watch');
+//     grunt.registerTask('default',['watch']);
+// }
+
 module.exports = function(grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      sass: {
-        dist: {
-          options: {
-            sourcemap: false,
-            compress: false,
-            yuicompress: false,
-            style: 'expanded',
-          },
-          files: {
-            'src/js/scss/style.css' : 'src/js/scss/styles.scss'
-          }
+
+  // Load required plugins
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Configure tasks
+  grunt.initConfig({
+    copy: {
+      dist: {
+        files: [
+          // Copy index.html directly
+          { src: 'src/index.html', dest: 'dist/index.html' },
+          // Copy js folder and its contents
+          { expand: true, cwd: 'src/js', src: ['**'], dest: 'dist/js' }
+        ]
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          // Enable source maps for easier debugging
+          sourceMap: true
         },
-      },
-      watch: {
-        css: {
-          files: '**/*.scss',
-          tasks: ['sass']
+        files: {
+          'dist/css/styles.css': 'src/js/scss/styles.scss'
         }
       }
-    });
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default',['watch']);
-}
+    }
+  });
+
+  // Register tasks
+  grunt.registerTask('compile-scss', ['sass:dist']);
+  grunt.registerTask('default', ['copy', 'compile-scss']);
+};
